51679581a8cfc2267ed3c4cf1eb66636b9ac52df
Rick Mason <richard.mason@eastsussex.gov.uk>
2018-03-02 10:53:53 +0000

    Add new MVC version of the address lookup control

--- Escc.WebsiteStyleGuide/Global.asax.cs
+++ Escc.WebsiteStyleGuide/Global.asax.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Escc.WebsiteStyleGuide.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
@@ -13,6 +15,7 @@ namespace Escc.WebsiteStyleGuide
 
         protected void Application_Start(object sender, EventArgs e)
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
 
