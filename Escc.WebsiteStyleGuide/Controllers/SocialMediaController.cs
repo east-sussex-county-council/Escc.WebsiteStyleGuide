b152c550d5a1319a461446168181d53c27c88055
Rick Mason <richard.mason@eastsussex.gov.uk>
2018-10-09 13:15:30 +0100

    Convert social media page to MVC. Add demo of East Sussex 1Space widget.

---
+++ Escc.WebsiteStyleGuide/Controllers/SocialMediaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Escc.WebsiteStyleGuide.Controllers
+{
+    public class SocialMediaController : Controller
+    {
+        // GET: SocialMedia
+        public ActionResult Index()
+        {
+            var model = new StyleGuidePage() { ShowEastSussex1SpaceWidget = true };
+            return View(model);
+        }
+    }
+}
